60a173a781cc66a69d7bd3e0ce025453aa0bbb0c
ComPilErr <compilerr@ya.ru>
2020-05-02 13:15:51 +0300

    Update run_forest_run.py

--- run_forest_run.py
+++ run_forest_run.py
@@ -8,7 +8,7 @@ MAX_SPEED = 2000
 MIN_SPEED = 50
 NULL_SPEED = 48
 PIN_OUT = 5
-Dt = 10
+Dt = 16
 increment = 30
 NO_TELEM = 0
 ON_TELEM = 1
@@ -23,9 +23,9 @@ for _ in range(10000):
     time.sleep(0.00012)
 
 while(1):
-    dshot.send(speed, PIN_OUT, NO_TELEM)
     t = time.time()*1000
     if (abs(t-t1)>Dt):
+        dshot.send(speed, PIN_OUT, NO_TELEM)
         t1=t
         speed += increment
         if(increment>0):
@@ -41,9 +41,11 @@ while(1):
         if(speed<MIN_SPEED):
             increment = (-1)*increment
             speed += increment
-            dshot.send(NULL_SPEED, PIN_OUT, ON_TELEM)
+            dshot.send(NULL_SPEED, PIN_OUT, NO_TELEM)
             time.sleep(0.00012)
             speed += increment
             for _ in range(9999):
                 dshot.send(NULL_SPEED, PIN_OUT, NO_TELEM)
                 time.sleep(0.00012)
+            break
+
